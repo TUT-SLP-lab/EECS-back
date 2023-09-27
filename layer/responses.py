fbdf855cf8e8ffd8d8fc6f28e9a192726aab7021
Hansyo <hansyo.souti.1999@gmail.com>
2023-09-27 17:28:09 +0900

    update: add response to header & origin from ClientURL

--- layer/responses.py
+++ layer/responses.py
@@ -1,29 +1,33 @@
+import os
+
+CLIENT_URL = os.environ["CLIENT_URL"]
+
 GET_HEADER = {
-    # "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Origin": "localhost:3001",
+    "Access-Control-Allow-Origin": CLIENT_URL,
     "Access-Control-Allow-Methods": "GET",
-    "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN",
+    "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN,Authorization",
+    "Access-Control-Allow-Credentials": "true",
 }
 
 POST_HEADER = {
-    # "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Origin": "localhost:3001",
+    "Access-Control-Allow-Origin": CLIENT_URL,
     "Access-Control-Allow-Methods": "POST",
-    "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN",
+    "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN,Authorization",
+    "Access-Control-Allow-Credentials": "true",
 }
 
 PUT_HEADER = {
-    # "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Origin": "localhost:3001",
+    "Access-Control-Allow-Origin": CLIENT_URL,
     "Access-Control-Allow-Methods": "PUT",
-    "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN",
+    "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN,Authorization",
+    "Access-Control-Allow-Credentials": "true",
 }
 
 DELETE_HEADER = {
-    # "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Origin": "localhost:3001",
+    "Access-Control-Allow-Origin": CLIENT_URL,
     "Access-Control-Allow-Methods": "DELETE",
-    "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN",
+    "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN,Authorization",
+    "Access-Control-Allow-Credentials": "true",
 }
 
 
@@ -31,7 +35,7 @@ def send_response(status_code, body, header):
     return {
         "statusCode": status_code,
         "body": body,
-        # "headers": header,
+        "headers": header,
     }
 
 
